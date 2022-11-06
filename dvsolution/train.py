08eff25d910b349be85ef51d5e7b40ef238ec62b
danyanyam <dvbuchko@edu.hse.ru>
2022-11-06 21:19:38 +0300

    chages

--- dvsolution/train.py
+++ dvsolution/train.py
@@ -12,10 +12,10 @@ def prepare_features(folder: Path) -> np.ndarray:
     logger.debug('prepairing features')
 
     data = h5py.File(str(folder / 'data.h5'), 'r')
-    mid_price = (
+    mid_price = ((
         np.array(data['OB/Ask']).min(axis=1) +
         np.array(data['OB/Bid']).max(axis=1)
-    ) / 2
+    ) / 2).reshape(-1, 1)
 
     return np.hstack(
         (
@@ -41,6 +41,18 @@ def eval(
     X_oot: np.ndarray,
     y_oot: np.ndarray
 ) -> Dict[str, float]:
+    """Evaluates the model, provided data
+
+    Args:
+        model (cb.CatBoostRegressor): model to evaluate
+        X_val (np.ndarray): validation set
+        y_val (np.ndarray): validation target
+        X_oot (np.ndarray): out of time set
+        y_oot (np.ndarray): out of time target
+
+    Returns:
+        Dict[str, float]: R2 of model
+    """
     logger.debug('evaluating model')
     return {
         'val': r2_score(y_val, model.predict(X_val)),
@@ -51,9 +63,18 @@ def eval(
 
 
 def prepare_model(task_type: str = 'CPU') -> cb.CatBoostRegressor:
-    logger.debug('preparing model')
+    """Initializes model with specified task_type
+
+    Args:
+        task_type (str, optional): Which device to use. Either 'CPU' or 'GPU'.
+            Defaults to 'CPU'.
+
+    Returns:
+        cb.CatBoostRegressor: initted model
+    """
+    logger.debug('prepairing model')
     return cb.CatBoostRegressor(
-        depth=7,
+        depth=6,
         verbose=50,
         random_seed=69,
         eval_metric='R2',
@@ -64,6 +85,17 @@ def prepare_model(task_type: str = 'CPU') -> cb.CatBoostRegressor:
 
 
 def train(folder: Path, task_type: str = 'CPU') -> None:
+    """Trains model, given data in folder of the following structure:
+
+    folder
+        - data.h5
+        - result.h5
+
+    Args:
+        folder (Path): path to data
+        task_type (str, optional): Which device to use. Either 'CPU' or 'GPU'
+            Defaults to 'CPU'.
+    """
     logger.info(f'training for folder {folder} has started!')
     X = prepare_features(folder)
     _, y = prepare_target(folder)
@@ -77,4 +109,4 @@ def train(folder: Path, task_type: str = 'CPU') -> None:
 
     print(eval(model, X_test, y_test, X_oot, y_oot))
     model.save_model("trained_model")
-    logger.inf(f'Training ended! Model has been saved to ./trained_model')
+    logger.info(f'Training ended! Model has been saved to ./trained_model')
